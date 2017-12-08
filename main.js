a2caea09f72f69d364713747dc4876c9f404cc87
joey-dafforn <jdafforn@purdue.edu>
2017-12-08 01:16:22 -0500

    updates

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
 	next();
 });
 app.get("/",function(req,res){
-	return res.sendFile(__dirname + "signin.html");
+	return res.sendFile(__dirname + "/signin.html");
 });
 app.post("/signin", function(request, response) {
 
